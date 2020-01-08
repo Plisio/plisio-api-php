1f7c6ca2253c2a47e29ff1e88fd3db270b84e17e
Plisio <plisiosmm@gmail.com>
2020-01-08 19:21:04 +0300

    init PHP Plisio library

---
+++ README.md
@@ -0,0 +1,50 @@
+## Plisio PHP API library
+
+To start using cryptocurrencies on your site you need to create an account on <https://plisio.net> and approve your domain under API setting page to get a **secret key**.
+
+This secret key will be used for creating instance of the PlisioClient.
+
+### Installation
+
+**Composer**
+
+You can install library via [Composer](http://getcomposer.org/). Run the following command in your terminal:
+
+```bash
+composer require plisio/plisio-api-php
+```
+
+Usage example:
+
+```
+$secretKey = 'xxxxx';
+
+$plisio = new \Plisio\ClientAPI($secretKey);
+$plisio->getBalances('BTC');
+```
+
+New order:
+
+```
+
+$request = [
+          'amount' => float, // required Invoice amount in selected currency OR amount_usd can be used instead
+//          'amount_usd' => float, // required Invoice amoint in USD
+          'currency' => string, // required (ETH, BTC, LTC, TZEC, DOGE, BCH, ...)
+          'order_number' => string, // required Client's internal ID
+          'order_name' => string, // required Client's internal name
+          'description' => string, // optional any description
+          'callback_url' => string, // optional Absolute URL where Plisio will send updates
+          'success_url' => string, // optional Absolute URL of the final (success) invoice link
+          
+];
+
+$plisio = new \Plisio\ClientAPI($secretKey);
+$invoice = $plisio->createTransaction($request);
+
+if ($invoice && isset($invoice['status']) && $invoice['status'] == 'success') {
+   header('Location: ' . $invoice['data']['invoice_url']);
+} else {
+  print_r($invoice);
+}
+```
---
+++ src/ClientAPI.php
@@ -0,0 +1,132 @@
+<?php
+namespace Plisio;
+
+class ClientAPI
+{
+    protected $secretKey = '';
+    public $apiEndPoint = 'https://plisio.net/api/v1';
+
+
+    public function __construct($secretKey)
+    {
+        $this->secretKey = $secretKey;
+    }
+
+    protected function getApiUrl($commandUrl)
+    {
+        return trim($this->apiEndPoint, '/') . '/' . $commandUrl;
+    }
+
+    public function getBalances($currency)
+    {
+        return $this->apiCall('balances', array('currency' => $currency));
+    }
+
+    public function createTransaction($req)
+    {
+        return $this->apiCall('invoices/new', $req);
+    }
+
+
+    /**
+     * Creates a withdrawal from your account to a specified address.<br />
+     * @param amount The amount of the transaction (floating point to 8 decimals).
+     * @param currency The cryptocurrency to withdraw.
+     * @param address The address to send the coins to.
+     * @param auto_confirm If auto_confirm is TRUE, then the withdrawal will be performed without an email confirmation.
+     */
+    public function createWithdrawal($amount, $currency, $address)
+    {
+        $req = array(
+            'currency' => $currency,
+            'amount' => $amount,
+            'to' => $address,
+            'type' => 'cash_out',
+        );
+        return $this->apiCall('operations/withdraw', $req);
+    }
+
+    /**
+     * Creates a withdrawal from your account to a specified address.<br />
+     * @param payments array of addresses and amounts.
+     * @param currency The cryptocurrency to withdraw.
+     */
+    public function createMassWithdrawal($payments, $currency)
+    {
+        $req = array(
+            'currency' => $currency,
+            'amount' => implode(',', array_values($payments)),
+            'to' => implode(',', array_keys($payments)),
+            'type' => 'mass_cash_out',
+        );
+        return $this->apiCall('operations/withdraw', $req);
+    }
+
+
+    private function isSetup()
+    {
+        return !empty($this->secretKey);
+    }
+
+    protected function getCurlOptions($url)
+    {
+        return [
+            CURLOPT_URL => $url,
+            CURLOPT_HTTPGET => true,
+            CURLOPT_FAILONERROR => false,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_HEADER => true,
+            CURLOPT_HTTPHEADER => [
+                'Content-Type: application/json',
+                'Accept: application/json'
+            ]
+        ];
+    }
+
+    private function apiCall($cmd, $req = array())
+    {
+        if (!$this->isSetup()) {
+            return array('error' => 'You have not called the Setup function with your private and public keys!');
+        }
+
+        // Generate the query string
+        $post_data = http_build_query($req, '', '&');
+        $queryString = '?api_key=' . $this->secretKey . '&' . $post_data;
+
+        try {
+            $apiUrl = $this->getApiUrl($cmd . $queryString);
+
+            $ch = curl_init();
+            curl_setopt_array($ch, $this->getCurlOptions($apiUrl));
+            $data = curl_exec($ch);
+
+            if ($data !== FALSE) {
+                $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+                $body = substr($data, $header_size);
+                $dec = $this->jsonDecode($body);
+                if ($dec !== NULL && count($dec)) {
+                    return $dec;
+                } else {
+                    // If you are using PHP 5.5.0 or higher you can use json_last_error_msg() for a better error message
+                    return array('status' => 'error', 'message' => 'Unable to parse JSON result (' . json_last_error() . ')');
+                }
+            } else {
+                return array('status' => 'error', 'message' => 'cURL error: ' . curl_error($ch));
+            }
+        } catch (\Exception $e) {
+            return array('status' => 'error', 'message' => 'Could not send request to API : ' . $apiUrl);
+        }
+    }
+
+    private function jsonDecode($data)
+    {
+        if (PHP_INT_SIZE < 8 && version_compare(PHP_VERSION, '5.4.0') >= 0) {
+            // We are on 32-bit PHP, so use the bigint as string option. If you are using any API calls with Satoshis it is highly NOT recommended to use 32-bit PHP
+            $dec = json_decode($data, TRUE, 512, JSON_BIGINT_AS_STRING);
+        } else {
+            $dec = json_decode($data, TRUE);
+        }
+        return $dec;
+    }
+}
